260fc6cca323ee6227184f459472d86b2d00c7d4
Abhai Kollara <abhai.dilip@gmail.com>
2020-01-29 13:38:10 +0530

    Added boolean parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -382,3 +382,34 @@ func testInfixExpression(t *testing.T, exp ast.Expression, left interface{},
 	}
 	return true
 }
+
+func TestBooleanExpression(t *testing.T) {
+	input := "true;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Boolean)
+	if !ok {
+		t.Fatalf("exp not *ast.Boolean. got=%T", stmt.Expression)
+	}
+
+	if ident.Value != true {
+		t.Errorf("ident.Value not %t. got=%t", true, ident.Value)
+	}
+
+	if ident.TokenLiteral() != "true" {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", "true", ident.TokenLiteral())
+	}
+}
